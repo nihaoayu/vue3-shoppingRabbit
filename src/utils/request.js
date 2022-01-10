6978194d8ea15617c581765e6f34d64483faaaf4
啊宇 <2961062796@qq.com>
2022-01-10 14:46:32 +0800

    配置axios及拦截器

---
+++ src/utils/request.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import store from '@/store'
+import router from '@/router'
+// baseURL 超时时间配置
+const instance = axios.create({
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net'
+})
+
+// 全局注入token
+instance.interceptors.request.use(config => {
+  const { token } = store.state.user.profile
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, e => Promise.reject(e))
+
+// 处理返回数据 token失效跳回到登录页
+instance.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      const redirectUrl = router.currentRoute.value.fullPath
+      router.replace(`/login?redirectUrl=${redirectUrl}`)
+    }
+    return Promise.reject(error)
+  }
+)
