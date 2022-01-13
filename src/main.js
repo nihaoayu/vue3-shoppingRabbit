2266fe2e7332f91c87c5325701899f53bb752053
啊宇 <2961062796@qq.com>
2022-01-13 15:58:21 +0800

    重置样式与公共样式

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入重置样式的文件
+import 'normalize.css'
+import '@/styles/common.less'
 
 createApp(App).use(store).use(router).mount('#app')
