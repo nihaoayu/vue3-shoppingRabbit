e772a3a5ee0d3f98a5c74b613faecf7dcec7d664
啊宇 <2961062796@qq.com>
2022-01-13 15:44:33 +0800

    公共布局和首页的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+// 导入公共布局
+import Layout from '@/views/Layout'
+import Home from '@/views/home'
 const routes = [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Home
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
