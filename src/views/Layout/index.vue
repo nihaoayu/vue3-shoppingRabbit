0a7d93470dbaed5a9b8725b45cdb7fc1cb9aaf82
啊宇 <2961062796@qq.com>
2022-01-13 16:09:13 +0800

    头部导航栏布局

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav>顶部通栏</nav>
+  <!-- 头部导航栏组件 -->
+  <XtxTopNav />
   <header>头部</header>
   <main>
     <!-- 二级路由出口 -->
@@ -9,8 +10,12 @@
 </template>
 
 <script>
+import XtxTopNav from './components/topnav'
 export default {
-  name: 'xtx-layout'
+  name: 'xtx-layout',
+  components: {
+    XtxTopNav
+  }
 }
 </script>
 
