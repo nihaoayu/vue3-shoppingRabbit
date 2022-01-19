af1c4f43451622cfcf8794a4b02a7cdd6446697c
啊宇 <2961062796@qq.com>
2022-01-19 15:53:34 +0800

    开启可以IP和域名访问权限

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,9 @@ module.exports = {
         path.join(__dirname, '/src/styles/variables.less')
       ]
     }
+  },
+  // 这个是给webpack-dev-server开启可以IP和域名访问权限
+  chainWebpack: config => {
+    config.devServer.disableHostCheck(true)
   }
 }
